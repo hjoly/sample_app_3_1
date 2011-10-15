7e3f6ef60e5ceeb7b6fbcf6986b6093b38bd5600
Hugues Joly <hjoly2003@yahoo.ca>
2011-10-15 13:39:14 -0400

    Final commit after successful migration to rails 3.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,19 +35,7 @@ class User < ActiveRecord::Base
 
   # Return true if the user's password matches the submitted password.
   def has_password?(submitted_password)
-    self.salt = make_salt if new_record?
     encrypted_password == encrypt(submitted_password)
-  end
-
-  def self.authenticate(email, submitted_password)
-    user = find_by_email(email)
-    return nil  if user.nil?
-    return user if user.has_password?(submitted_password)
-  end
-
-  def self.authenticate_with_salt(id, cookie_salt)
-    user = find_by_id(id)
-    (user && user.salt == cookie_salt) ? user : nil
   end
 
   def following?(followed)
@@ -66,9 +54,22 @@ class User < ActiveRecord::Base
     Micropost.from_users_followed_by(self)
   end
 
+  class << self
+    def authenticate(email, submitted_password)
+      user = find_by_email(email)
+      (user && user.has_password?(submitted_password))? user : nil
+    end
+
+    def authenticate_with_salt(id, cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user : nil
+    end
+  end
+
   private
 
    def encrypt_password
+     self.salt = make_salt if new_record?
      self.encrypted_password = encrypt(password)
    end
 
